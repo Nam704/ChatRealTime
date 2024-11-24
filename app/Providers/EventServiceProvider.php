e1a1198236bc4060425391e90087d40c3af7187f
Nam704 <daovannam2k4@gmail.com>
2024-11-25 01:29:57 +0700

    Remove .history files from repository

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Events\UserSessionChanged;
+use App\Listeners\BroadcastUserLoginNotification;
+use App\Listeners\BroadcastUserLogoutNotification;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -9,14 +14,16 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
+
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        Login::class => [
+            BroadcastUserLoginNotification::class
+        ],
+        Logout::class => [
+            BroadcastUserLogoutNotification::class
         ],
     ];
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import "./bootstrap";
+console.log(Echo);
+Echo.private("notification")
+    .listen("UserSessionChanged", (e) => {
+        console.log(e);
+        const notiElement = document.getElementById("notification");
+        notiElement.innerText = e.message;
+        notiElement.classList.remove("invisible");
+        notiElement.classList.remove("alert-success");
+        notiElement.classList.remove("alert-danger");
+        notiElement.classList.add("alert-" + e.type);
+    })
+    .error((error) => {
+        console.error("Echo error:", error);
+    });
